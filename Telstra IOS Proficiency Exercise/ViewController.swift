42d3396dfc8643cf03fd13b5d2f24fb5f85d8ab1
Sarathi Barik, Partha <partha.sarathi.barik@accenture.com>
2020-01-13 17:03:25 +0530

    Initial Codebase

---
+++ Telstra IOS Proficiency Exercise/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Telstra IOS Proficiency Exercise
+//
+//  Created by partha.sarathi.barik on 13/01/20.
+//  Copyright © 2020 partha.sarathi.barik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
