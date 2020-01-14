69b6df731836e4910d829ebd186c2a7fd15c4eb6
Sarathi Barik, Partha <partha.sarathi.barik@accenture.com>
2020-01-14 10:31:41 +0530

    Telstra Image Downloader added to download images. Image Download cache added to manager image downloads.

--- Telstra IOS Proficiency Exercise/TelstraTableViewController.swift
+++ Telstra IOS Proficiency Exercise/TelstraTableViewController.swift
@@ -8,16 +8,23 @@
 
 import UIKit
 
+//MARK: - Telstra Table View Controller Class
 class TelstraTableViewController: UITableViewController {
-
+    
+    //MARK: - Telstra Table View Controller Class veriables
+    
     var dataSet : [TelstraDataSet] = []
     let cellId = "cellId"
-
+    var imageDownloader : TelstraImageDownloader?
+    
+    //MARK: - Telstra Table View member functions
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         loadTelstraData()
         tableView.register(TelstraTableViewCell.self, forCellReuseIdentifier: cellId)
         tableView.allowsSelection = false
+        imageDownloader = TelstraImageDownloader()
     }
     
     ///Downloads Telstra JSON dataset and ready it for the tableview
@@ -30,10 +37,11 @@ class TelstraTableViewController: UITableViewController {
     }
 }
 
+//MARK: - Extension for Telstra Table View Controller
 extension TelstraTableViewController {
     
     // MARK: - Telstra Tableview data source
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // Returns the Number of Data Set with valid titles.
         return dataSet.count
@@ -45,16 +53,34 @@ extension TelstraTableViewController {
         let currentItem = dataSet[indexPath.row]
         cell.cellTitle.text = currentItem.title
         cell.cellDescription.text = currentItem.description
+        cell.cellImage.image = UIImage(named: "noImg")
         return cell
     }
     
     // MARK: - Telstra Tableview Delegates
-
+    
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         let currentItem = dataSet[indexPath.row]
         guard let heightOfDescription = currentItem.description?.height(withConstrainedWidth: tableView.frame.width, font: UIFont.systemFont(ofSize: 16)) else {return 100}
         return heightOfDescription + 100
     }
     
-    
+    /// Downloads the images which are being shown on the screen at the moment.
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let cell = cell as! TelstraTableViewCell
+        let currentItem = dataSet[indexPath.row]
+        if let imageUrl = currentItem.imageHref {
+            DispatchQueue.global(qos: .default).async {
+                self.imageDownloader?.getTelstraImage(with: imageUrl, completionHandler: { (image) in
+                    DispatchQueue.main.async {
+                        if tableView.cellForRow(at: indexPath) != nil {
+                            print(indexPath.row)
+                            print(imageUrl)
+                            cell.cellImage.image = image
+                        }
+                    }
+                })
+            }
+        }
+    }
 }
